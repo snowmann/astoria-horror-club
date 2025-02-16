e13e367cfd223199b475b74c9c9ccec1bd93b020
Tom Herrmann <tomherrmanndev@gmail.com>
2025-02-16 15:31:44 -0500

    update events

--- src/collections/Events.ts
+++ src/collections/Events.ts
@@ -2,6 +2,25 @@ import { isAdmin } from '@/access/isAdmin'
 import { isOrganizer } from '@/access/isOrganizer'
 import type { CollectionConfig, PayloadRequest } from 'payload'
 
+// Array - for repeating content, supports nested fields
+// Blocks - for block-based content, supports nested fields
+// Checkbox - saves boolean true / false values
+// Code - renders a code editor interface that saves a string
+// Date - renders a date picker and saves a timestamp
+// Email - ensures the value is a properly formatted email address
+// Group - nests fields within a keyed object
+// JSON - renders a JSON editor interface that saves a JSON object
+// Number - saves numeric values
+// Point - for location data, saves geometric coordinates
+// Radio - renders a radio button group that allows only one value to be selected
+// Relationship - assign relationships to other collections
+// Rich Text - renders a fully extensible rich text editor
+// Select - renders a dropdown / picklist style value selector
+// Tabs (Named) - similar to group, but renders nested fields within a tabbed layout
+// Text - simple text input that saves a string
+// Textarea - similar to text, but allows for multi-line input
+// Upload - allows local file and image upload
+
 export const Events: CollectionConfig = {
   slug: 'events',
   admin: {
