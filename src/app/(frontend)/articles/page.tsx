a0b648ed64699c8de5c051bfe1eaf334b587b0d3
Tom Herrmann <tomherrmanndev@gmail.com>
2025-02-21 15:25:55 -0500

    remove breaking payload call

--- src/app/(frontend)/articles/page.tsx
+++ src/app/(frontend)/articles/page.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import { getPayload } from 'payload'
-import config from '@payload-config'
 
-const payload = await getPayload({ config })
-
-export default function AritclesFeed() {
+export default async function AritclesFeed() {
   return <h1>Articles Go Here</h1>
 }
