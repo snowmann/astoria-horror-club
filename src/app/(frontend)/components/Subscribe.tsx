0d75a0c2ff33be1f06db9489fc655b49fcff4cd2
Mary Snow <snow.mary.r@gmail.com>
2025-03-02 20:35:19 -0500

    add sign up form to home page

--- src/app/(frontend)/components/Subscribe.tsx
+++ src/app/(frontend)/components/Subscribe.tsx
@@ -1,3 +1,102 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/components/ui/form'
+import { z } from 'zod'
+
 export default function Subscribe() {
-  return <h1 className="text-3xl font-bold underline">Subscribe</h1>
+  const formSchema = z.object({
+    firstName: z.string().min(2, {
+      message: 'Required',
+    }),
+    lastName: z.string().min(2, {
+      message: 'Required',
+    }),
+    email: z.string().email({
+      message: 'Invalid email address.',
+    }),
+  })
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // TODO: Handle form submission
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field: formField }) => (
+              <FormItem className="pb-8">
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="First Name" {...formField} />
+                </FormControl>
+                <div className="relative">
+                  <FormMessage className="absolute -top-6 left-0 w-full" />
+                </div>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field: formField }) => (
+              <FormItem className="pb-8">
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Last Name" {...formField} />
+                </FormControl>
+                <div className="relative">
+                  <FormMessage className="absolute -top-6 left-0 w-full" />
+                </div>
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid grid-cols-1">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field: formField }) => (
+              <FormItem className="pb-8">
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...formField} />
+                </FormControl>
+                <div className="relative">
+                  <FormMessage className="absolute -top-6 left-0 w-full" />
+                </div>
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex justify-end">
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
+    </Form>
+  )
 }
