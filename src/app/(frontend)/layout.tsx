acf042bdd51bc4e7cd496931803ff275a83f8ca2
Tom Herrmann <tomherrmanndev@gmail.com>
2025-03-09 12:02:21 -0400

    update styling

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './globals.css'
+import Header from './components/Header'
 
 export const metadata = {
   description: 'The official site of Astoria Horror Club.',
@@ -10,9 +11,11 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>
+        <Header />
         <main>{children}</main>
+        {/* FOOTER */}
       </body>
     </html>
   )
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import './styles.css'
+import './globals.css'
 import SubscriberForm from './components/SubscriberForm'
-import Header from './components/Header'
 
 export default async function HomePage() {
   return (
     <div className="home">
-      <Header />
       <div className="content">
         <SubscriberForm />
       </div>
