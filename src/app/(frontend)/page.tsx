8f9ce6fb616281bd1807cc7eb98af1d83672e56d
Tom Herrmann <tomherrmanndev@gmail.com>
2025-03-16 15:58:06 -0400

    social links

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './globals.css'
 import SubscriberForm from './components/SubscriberForm'
+import { SocialLinks } from './components/SocialLinks'
 
 export default async function HomePage() {
   return (
     <div className="home">
       <div className="content">
         <SubscriberForm />
+        <SocialLinks />
       </div>
     </div>
   )
